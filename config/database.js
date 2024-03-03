a4f7607e789789e68129dc30cbfcb4b14492fc9a
Axel <axel_clemente@yahoo.com>
2024-03-03 11:06:01 +0100

    database connected to cloud server

--- config/database.js
+++ config/database.js
@@ -1,19 +1,25 @@
+// Conexion local
 // const mongoose = require('mongoose');
 
-//  mongoose
-//   .connect('mongodb://localhost/Axel')
-//   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-//   .catch(err => console.error('Error connecting to mongo', err));
+// const connectDatabase = () => {
+//   mongoose
+//     .connect('mongodb://localhost/Axel')
+//     .then(() => console.log(`Connected to Mongo! Database name: "Axel"`))
+//     .catch(err => console.error('Error connecting to mongo', err));
+// };
 
-// config/database.js
-// config/database.js
+// module.exports = connectDatabase;
+
+//Conexion en la nube
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
+  const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/?retryWrites=true&w=majority&appName=Cluster0`;
+
   mongoose
-    .connect('mongodb://localhost/Axel')
-    .then(() => console.log(`Connected to Mongo! Database name: "Axel"`))
-    .catch(err => console.error('Error connecting to mongo', err));
+    .connect(uri)
+    .then(() => console.log('Connected to MongoDB Atlas working'))
+    .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
 };
 
 module.exports = connectDatabase;
