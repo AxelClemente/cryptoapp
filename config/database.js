967026c87ee693a3c916dd60ed18ee4ab0143c44
Axel <axel_clemente@yahoo.com>
2024-03-09 15:19:51 +0100

    Test v1

--- config/database.js
+++ config/database.js
@@ -11,34 +11,15 @@
 // module.exports = connectDatabase;
 
 //Conexion en la nube
-// const mongoose = require('mongoose');
-
-// const connectDatabase = () => {
-//   const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/?retryWrites=true&w=majority&appName=Cluster0`;
-
-//   mongoose
-//     .connect(uri)
-//     .then(() => console.log('Connected to MongoDB Atlas working'))
-//     .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
-// };
-
-// module.exports = connectDatabase;
-
 const mongoose = require('mongoose');
 
-const connectDatabase = async () => {
-  const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const connectDatabase = () => {
+  const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_URL}/?retryWrites=true&w=majority&appName=Cluster0`;
 
-  try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Connected to MongoDB Atlas working');
-  } catch (err) {
-    console.error('Error connecting to MongoDB Atlas:', err);
-    process.exit(1); // Salimos de la aplicación si no hay conexión a la base de datos
-  }
+  mongoose
+    .connect(uri)
+    .then(() => console.log('Connected to MongoDB Atlas working'))
+    .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
 };
 
 module.exports = connectDatabase;
--- server/server.js
+++ server/server.js
@@ -1,22 +1,5 @@
-const app = require('../app');
-const mongoose = require('mongoose'); // Asegúrate de importar mongoose
-const PORT = process.env.PORT || 3000;
+const app = require ('../app')
 
-// Función para conectar a la base de datos
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI); // Usa la variable de entorno MONGO_URI para tu cadena de conexión
-    console.log('MongoDB Connected');
-
-    // Iniciar el servidor después de establecer la conexión con la base de datos
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    process.exit(1); // Sale del proceso si no puede conectarse a MongoDB
-  }
-};
-
-// Inicia el proceso de conexión a la base de datos
-connectDB();
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
