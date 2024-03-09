0510f3d13d57ecf1ed88757f21f9281bda882164
Axel <axel_clemente@yahoo.com>
2024-03-09 11:03:08 +0100

    CORS PROBLEMS origin netlify añadido

--- app.js
+++ app.js
@@ -10,59 +10,24 @@ const holdingsRoutes = require('./routes/holdings.routes');
 const app = express();
 const routes = require('./routes/routes');
 
-
-
-
 // Conectar a la base de datos
 connectDatabase();
 
 // Usar las rutas definidas
 app.use(routes);
 app.use(express.json()); // Middleware para parsear JSON
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:3001', process.env.ORIGIN] // Aquí se agregan los orígenes permitidos
+  }));
+  
 app.use(express.static('public'));
 
 // Rutas de autenticación
 app.use("/auth", authRoutes);
 app.use("/portfolio", portfolioRoutes)
 app.use('/api', holdingsRoutes); // Asegúrate de que 'api' esté en la ruta
-
-
-
-
-
-///////////////////////////////////////////////////// test
-// Definir un modelo simple
-// const TestModel = mongoose.model('Test', new mongoose.Schema({ name: String }));
-
-// Crear un documento de prueba
-// TestModel.create({ name: 'Test Entry' })
-//   .then(doc => {
-//     console.log('Documento creado:', doc);
-//     // Leer el documento
-//     return TestModel.findOne({ name: 'Test Entry' });
-//   })
-//   .then(doc => {
-//     console.log('Documento encontrado:', doc);
-//     // Actualizar el documento
-//     return TestModel.updateOne({ name: 'Test Entry' }, { name: 'Updated Entry' });
-//   })
-//   .then(result => {
-//     console.log('Documento actualizado:', result);
-//     // Eliminar el documento
-//     return TestModel.deleteOne({ name: 'Updated Entry' });
-//   })
-// //   .then(result => {
-// //     console.log('Documento eliminado:', result);
-// //     // Cerrar la conexión de la base de datos
-// //     mongoose.connection.close();
-// //   })
-//   .catch(err => {
-//     console.error('Error durante las operaciones CRUD:', err);
-//     mongoose.connection.close();
-//   });
-///////////////////////////////////////////////////// fin test
-
 
 
 module.exports = app;
--- routes/portfolio.routes.js
+++ routes/portfolio.routes.js
@@ -5,32 +5,6 @@ const authenticateToken = require('../middlewares/authenticateToken');
 const jwt = require('jsonwebtoken');
 
 
-// router.post('/add', authenticateToken, async (req, res) => {
-//   const { userId, cryptoId, amount } = req.body;
-  
-//   try {
-//     // Asumiendo que ya tienes un documento de portafolio creado para este usuario,
-//     // de lo contrario, necesitarías crear uno.
-//     let portfolio = await Portfolio.findOne({ userId: userId });
-
-//     if (!portfolio) {
-//       // Si no existe, crear un nuevo documento de portafolio
-//       portfolio = new Portfolio({
-//         userId,
-//         cryptos: [{ id: cryptoId, amount: amount }],
-//       });
-//     } else {
-//       // Si ya existe, simplemente agrega la nueva criptomoneda
-//       portfolio.cryptos.push({ id: cryptoId, amount: amount });
-//     }
-
-//     const updatedPortfolio = await portfolio.save();
-//     res.status(200).json(updatedPortfolio);
-//   } catch (error) {
-//     res.status(500).json({ message: "Error adding crypto to portfolio", error: error.message });
-//   }
-// });
-
 router.post('/add', authenticateToken, async (req, res) => {
   const { userId, cryptoId, amount } = req.body;
   
