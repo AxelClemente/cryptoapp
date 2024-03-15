735737832f44532493b70d36308fa6741d81aec7
Axel <axel_clemente@yahoo.com>
2024-03-15 10:13:24 +0100

    testing signup and google v1

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Endpoint simplificado para propósitos de prueba
 router.get("/test-backend", (req, res) => {
     // Envía una respuesta simple para verificar si el endpoint funciona correctamente
-    res.status(200).json({ message: "Este es un mensaje de prueba desde el backend" });
+    res.status(200).json({ message: "Esto es un mensaje para seguir construyendo una gran app - Axel!" });
 });
 
 module.exports = router;
