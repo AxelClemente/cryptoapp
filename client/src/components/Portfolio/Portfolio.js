faecb028d84c58666a4b873d247e47442d4ca6a5
Axel <axel_clemente@yahoo.com>
2024-03-21 17:13:55 +0100

    portfolio page calculando ath y precios actuales

--- client/src/components/Portfolio/Portfolio.js
+++ client/src/components/Portfolio/Portfolio.js
@@ -96,19 +96,55 @@ const Portfolio = () => {
 
 
   return (
-    <div>
+    <div className="mx-[40px]">
       <button onClick={handleGoBack}>Volver atrás</button> {/* Botón para ir hacia atrás */}
       <h1>Mercados top 150 cryptomonedas</h1>
-      <ul>
+      {/* <ul>
         {cryptos.map((crypto) => (
           <li key={crypto.id}>
             <img src={crypto.image} alt={crypto.name} style={{ width: '20px', height: '20px' }} />
             <span>{crypto.name}: ${crypto.current_price}</span>
             <button onClick={() => handleOpenModal(crypto.id)}>Agregar</button>
             
+..
           </li>
         ))}
-      </ul>
+      </ul> */}
+      <ul role="list" className="divide-y divide-gray-100">
+          {cryptos.map((crypto) => (
+            <li key={crypto.name} className="flex justify-between gap-x-6 py-5">
+              <div className="flex min-w-0 gap-x-4">
+                <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={crypto.image} alt="" />
+                <div className="min-w-0 flex-auto">
+                  <p className="text-sm font-semibold leading-6 text-gray-900">{crypto.name}</p>
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">{crypto.current_price.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 8 })} $</p>
+                </div>
+              </div>
+              <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+              {/* <p className="text-sm leading-6 text-gray-900">ATH {crypto.ath.toLocaleString()} $</p> */}
+              {/* <p className="text-sm leading-6 text-gray-900">{crypto.ath.toLocaleString('es-ES')}$</p> */}
+              <p className="text-sm leading-6 text-gray-900">{crypto.ath.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 8 })}$</p>
+
+{crypto.ath ? (
+  <div className="mt-1 text-xs leading-5 text-gray-500">
+    24h <span style={{ color: crypto.price_change_percentage_24h < 0 ? 'red' : '#80e038' }}>
+      {crypto.price_change_percentage_24h.toFixed(1)}%
+    </span>
+    <button onClick={() => handleOpenModal(crypto.id)} className="ml-2 text-blue-500 hover:text-blue-700">Agregar</button>
+  </div>
+) : (
+  <div className="mt-1 flex items-center gap-x-1.5">
+    <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+      <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+    </div>
+    <p className="text-xs leading-5 text-gray-500">Online</p>
+  </div>
+)}
+
+              </div>
+            </li>
+          ))}
+        </ul>
       {showModal && (
         <Modal 
         onClose={() => setShowModal(false)}
