75be6dbff7051c87b822b040fbf09e234eb91afa
Axel <axel_clemente@yahoo.com>
2024-03-21 13:16:01 +0100

    Tailwind css added

--- client/src/components/TestBackendConnection.js
+++ client/src/components/TestBackendConnection.js
@@ -30,12 +30,17 @@ function TestBackendConnection() {
       navigate(-1); // Navega hacia atrás en el historial del navegador
     };
 
-  return (
-    <div>
-      <button onClick={handleGoBack}>Volver atrás</button> {/* Botón para ir hacia atrás */}
-      <h2>Respuesta del Backend:</h2>
-      <p>{responseMessage}</p>
-    </div>
+    return (
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100 p-4">
+        <button
+          onClick={handleGoBack}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
+        >
+          Volver atrás
+        </button>
+        <h2 className="mt-4 text-lg font-semibold text-gray-800">Respuesta del Backend:</h2>
+        <p className="text-gray-600">{responseMessage}</p>
+      </div>
   );
 }
 
---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ajusta a tus extensiones de archivos según sea necesario
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
