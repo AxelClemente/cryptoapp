35a95422a679fd87daafa8052ca4f0058e75ee40
Axel <axel_clemente@yahoo.com>
2024-03-30 10:26:10 +0100

    modal working with css

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,48 +1,83 @@
 // import React from 'react';
-// import './modal.css';
 
-// const Modal = ({ onClose, onConfirm, amount, setAmount, currentPrice, currentDate }) => {
+// const Modal = ({ onClose, onConfirm, amount, setAmount, currentPrice, cryptoName }) => {
 
-  
+//   // Función para capitalizar la primera letra
+//   const capitalizeFirstLetter = (string) => {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+//   };
 
-//   // Gestiona el envío del formulario
 //   const handleSubmit = (event) => {
 //     event.preventDefault();
-//     // Aquí puedes validar los datos si es necesario antes de llamar a onConfirm
 //     onConfirm();
 //   };
 
-//   return (
-//     <div className="modal">
-//       <div className="modal-content">
-//         <span className="close-button" onClick={onClose}>&times;</span>
-//         <h2>Add Transaction</h2>
-//         <form onSubmit={handleSubmit}>
-//           <div className="form-group">
-//             <label htmlFor="price">Price per coin *</label>
-//             <input
-//               id="price"
-//               type="text" // Cambiado a text para que sea de solo lectura
-//               value={currentPrice} // Utiliza el precio actual
-//               readOnly // Hace el campo de solo lectura
-//             />
-//           </div>
-//           <div className="form-group">
-//             <label htmlFor="quantity">Quantity *</label>
-//             <input
-//               id="quantity"
-//               type="number"
-//               value={amount}
-//               onChange={(e) => setAmount(e.target.value)}
-//               required
-//             />
-//           </div>
+//   const totalPrice = currentPrice * amount;
 
-//           <div className="form-actions">
-//             <button type="button" onClick={onClose}>Cancel</button>
-//             <button type="submit" className="submit-button">Submit</button>
+//   return (
+//     <div >
+//       <div >
+//         <div aria-hidden="true" onClick={onClose}>
+//           <div ></div>
+//         </div>
+
+//         {/* This element is to trick the browser into centering the modal contents. */}
+//         <span  aria-hidden="true">&#8203;</span>
+
+//         <div >
+//           <div>
+//             <div >
+//               <h3 >
+//                 Add {capitalizeFirstLetter(cryptoName)}
+//               </h3>
+//               <div >
+//                 <form onSubmit={handleSubmit}>
+//                   <div >
+//                     <label htmlFor="price" >Price per Coin</label>
+//                     <input
+//                       type="text"
+//                       name="price"
+//                       id="price"               
+//                       placeholder={currentPrice.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 8 })}
+//                       value={currentPrice}
+//                       readOnly
+//                     />
+//                   </div>
+//                   <div >
+//                     <label htmlFor="quantity" >Quantity</label>
+//                     <input
+//                       type="number"
+//                       name="quantity"
+//                       id="quantity"
+//                       placeholder={currentPrice.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 8 })}
+//                       value={amount}
+//                       onChange={(e) => setAmount(e.target.value)}
+//                     />
+//                   </div>
+//                   <div >
+//                     <label htmlFor="total" >Total Spent</label>
+//                     <input
+//                       type="text"
+//                       name="total"
+//                       id="total"                     
+//                       placeholder={totalPrice.toFixed(2).toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 8 })}
+//                       value={totalPrice.toFixed(2).toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 8 })}
+//                       readOnly
+//                     />
+//                   </div>
+//                   <div >
+//                     <button type="button" onClick={onClose} >
+//                       Cancel
+//                     </button>
+//                     <button type="submit" >
+//                       Submit
+//                     </button>
+//                   </div>
+//                 </form>
+//               </div>
+//             </div>
 //           </div>
-//         </form>
+//         </div>
 //       </div>
 //     </div>
 //   );
@@ -52,9 +87,9 @@
 
 
 import React from 'react';
+import '../Modal/modal.css'
 
 const Modal = ({ onClose, onConfirm, amount, setAmount, currentPrice, cryptoName }) => {
-
   // Función para capitalizar la primera letra
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -68,71 +103,59 @@ const Modal = ({ onClose, onConfirm, amount, setAmount, currentPrice, cryptoName
   const totalPrice = currentPrice * amount;
 
   return (
-    <div className="fixed z-10 inset-0 overflow-y-auto">
-      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <div className="fixed inset-0 transition-opacity" aria-hidden="true" onClick={onClose}>
-          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+    <div className="modal-overlay" onClick={onClose}>
+      {/* Este div sirve como fondo del modal y cerrará el modal si se hace clic fuera de .modal-content */}
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <div className="modal-header">
+          <h3 className="modal-title">
+            Add {capitalizeFirstLetter(cryptoName)}
+          </h3>
+          <button className="close-button" onClick={onClose}>&times;</button>
         </div>
-
-        {/* This element is to trick the browser into centering the modal contents. */}
-        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-
-        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-          <div>
-            <div className="mt-3 text-center sm:mt-0 sm:text-left">
-              <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-                Add {capitalizeFirstLetter(cryptoName)}
-              </h3>
-              <div className="mt-2">
-                <form onSubmit={handleSubmit}>
-                  <div className="mt-4">
-                    <label htmlFor="price" className="block text-sm font-medium text-gray-700">Price per Coin</label>
-                    <input
-                      type="text"
-                      name="price"
-                      id="price"
-                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
-                      placeholder={currentPrice.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 8 })}
-                      value={currentPrice}
-                      readOnly
-                    />
-                  </div>
-                  <div className="mt-4">
-                    <label htmlFor="quantity" className="block text-sm font-medium text-gray-700">Quantity</label>
-                    <input
-                      type="number"
-                      name="quantity"
-                      id="quantity"
-                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
-                      placeholder={currentPrice.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 8 })}
-                      value={amount}
-                      onChange={(e) => setAmount(e.target.value)}
-                    />
-                  </div>
-                  <div className="mt-4">
-                    <label htmlFor="total" className="block text-sm font-medium text-gray-700">Total Spent</label>
-                    <input
-                      type="text"
-                      name="total"
-                      id="total"
-                      className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-500 focus:ring-opacity-50"
-                      placeholder={totalPrice.toFixed(2).toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 8 })}
-                      value={totalPrice.toFixed(2).toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 8 })}
-                      readOnly
-                    />
-                  </div>
-                  <div className="mt-5 sm:mt-6">
-                    <button type="button" onClick={onClose} className="mr-3 py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                      Cancel
-                    </button>
-                    <button type="submit" className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                      Submit
-                    </button>
-                  </div>
-                </form>
-              </div>
+        <div className="modal-body">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="price">Price per Coin</label>
+              <input
+                type="text"
+                name="price"
+                id="price"               
+                placeholder={currentPrice.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 8 })}
+                value={currentPrice}
+                readOnly
+              />
             </div>
-          </div>
+            <div className="form-group">
+              <label htmlFor="quantity">Quantity</label>
+              <input
+                type="number"
+                name="quantity"
+                id="quantity"
+                placeholder="0"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="total">Total Spent $</label>
+              <input
+                type="text"
+                name="total"
+                id="total"                     
+                placeholder={totalPrice.toFixed(2).toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 8 })}
+                value={totalPrice.toFixed(2).toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 8 })}
+                readOnly
+              />
+            </div>
+            <div className="modal-actions">
+              <button type="button" className="button cancel" onClick={onClose}>
+                Cancel
+              </button>
+              <button type="submit" className="button submit">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
