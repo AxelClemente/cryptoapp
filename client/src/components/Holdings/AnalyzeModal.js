235a8f8697f6dd36539d6b64707b379a89e94803
Axel <axel_clemente@yahoo.com>
2024-04-18 19:44:17 +0200

    total source added

--- client/src/components/Holdings/AnalyzeModal.js
+++ client/src/components/Holdings/AnalyzeModal.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 const AnalyzeModal = ({ onClose, averagePrice, crypto, sources }) => {
+  // Función para agrupar y sumar las cantidades de las sources duplicadas
+  const aggregatedSources = sources.reduce((acc, source) => {
+    const existingSource = acc.find(item => item.source === source.source);
+    if (existingSource) {
+      existingSource.amount += source.amount;  // Suma la cantidad si la fuente ya existe
+    } else {
+      acc.push({ ...source });  // Agrega la nueva fuente al acumulador
+    }
+    return acc;
+  }, []);
+
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={e => e.stopPropagation()}>
@@ -12,8 +23,10 @@ const AnalyzeModal = ({ onClose, averagePrice, crypto, sources }) => {
           <p>Tu promedio de compra de {crypto.name} es {averagePrice.toLocaleString('es-ES', { style: 'currency', currency: 'USD' })}</p>
           <p>Están resguardados en:</p>
           <ul>
-            {sources.map(source => (
-              <li key={source.source}>{source.source.charAt(0).toUpperCase() + source.source.slice(1)}: {source.amount} unidades</li>
+            {aggregatedSources.map(source => (
+              <li key={source.source}>
+                {source.source.charAt(0).toUpperCase() + source.source.slice(1)}: {source.amount} unidades
+              </li>
             ))}
           </ul>
         </div>
@@ -23,6 +36,7 @@ const AnalyzeModal = ({ onClose, averagePrice, crypto, sources }) => {
 };
 
 export default AnalyzeModal;
+
 
 
 
