82c6315cc3b15436af802f25b2b705e2ca824ce7
Axel <axel_clemente@yahoo.com>
2024-05-19 21:26:00 +0200

    final average modal

--- client/src/components/Holdings/AverageModal.js
+++ client/src/components/Holdings/AverageModal.js
@@ -27,78 +27,39 @@
 
 // export default AverageModal;
 
+
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import '../Holdings/averageModal.css';
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
-
 function AverageModal({ onClose, data }) {
-  // Configuración de los datos para el gráfico de barras
-  const chartData = {
-    labels: data.map(crypto => crypto.id),
-    datasets: [
-      {
-        label: 'Value in USD',
-        data: data.map(crypto => crypto.value),
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  // Configuración de opciones para el gráfico
-  const chartOptions = {
-    responsive: true,
-    scales: {
-      x: {
-        title: {
-          display: true,
-          text: 'Cryptocurrency',
-        },
-      },
-      y: {
-        title: {
-          display: true,
-          text: 'USD',
-        },
-        beginAtZero: true,
-        ticks: {
-          callback: function(value) {
-            return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
-          }
-        }
-      },
-    },
-  };
-
   return (
     <div className="modal">
       <div className="modal-content">
         <span className="close" onClick={onClose}>&times;</span>
-        <h2 className='titleModal'>Holdings Breakdown</h2>
-        <div className="chart-container">
-          <Bar data={chartData} options={chartOptions} />
-        </div>
-        <ul>
-          {data.map((crypto) => (
-            <li key={crypto.id}>
-              <img src={crypto.image} alt={crypto.id} className="crypto-imagens" />
-              <span className="crypto-a"> {crypto.id}</span>
-              <span>
-                {crypto.value.toLocaleString('en-US', { style: 'currency', currency: 'USD' })} 
-                <span className="crypto-percentage"> {crypto.percentage.toFixed(2)}%</span>
-              </span>
-            </li>
-          ))}
-        </ul>
+        <h2 className='titleModal'>Initial Investement </h2>
+        <table className="average-table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>USD</th>
+              <th>%</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((crypto) => (
+              <tr key={crypto.id}>
+                <td><img src={crypto.image} alt={crypto.id} className="crypto-imagens" /></td>
+                <td>{crypto.id}</td>
+                <td>{crypto.value.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
+                <td>{crypto.percentage.toFixed(2)}%</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
 }
 
 export default AverageModal;
-
-
